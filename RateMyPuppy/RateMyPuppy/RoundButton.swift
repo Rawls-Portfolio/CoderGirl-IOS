ea4e06013f00b1d46979a28b72fe04c178f8ca08
Amanda Rawls <jennifer.amanda.rawls@monsanto.com>
2018-03-14 14:58:26 -0500

    add round button class

---
+++ RateMyPuppy/RateMyPuppy/RoundButton.swift
@@ -0,0 +1,29 @@
+import UIKit
+
+@IBDesignable class RoundButton: UIButton {
+    
+    @IBInspectable var cornerRadius: CGFloat {
+        get {
+            return self.layer.cornerRadius
+        }
+        set(radius) {
+            self.layer.cornerRadius = radius
+        }
+    }
+    
+    func defaultSettings(){
+        self.layer.backgroundColor = UIColor.lightGray.cgColor
+        self.titleLabel?.textColor = UIColor.purple
+        self.layer.cornerRadius = 10
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        defaultSettings()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        defaultSettings()
+    }
+}
